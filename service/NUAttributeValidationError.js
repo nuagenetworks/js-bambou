a96add56a9a1298410c1f0dd9591d14a6c0cec45
Natalia Balus <natalia.balus@nokia.com>
2017-07-14 15:28:21 -0700

    Flaten service

--- service/NUAttributeValidationError.js
+++
@@ -1,8 +0,0 @@
-import NUObject from './NUObject';
-
-export default class NUAttributeValidationError extends NUObject {
-    constructor(localName, remoteName, title, description) {
-        super();
-        this.defineProperties({ localName, remoteName, title, description });
-    }
-}
--- service/NUException.js
+++
@@ -1,9 +0,0 @@
-
-export default class NUException extends Error {
-    constructor(message) {
-        super();
-        this.message = message;
-        this.stack = (new Error()).stack;
-        this.name = this.constructor.name;
-    }
-}
