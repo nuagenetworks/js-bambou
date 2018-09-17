e0319bda4de1088b69bf0bfb63ea73911130afaa
Anitha <32629188+myTechPlayArea@users.noreply.github.com>
2018-09-17 14:13:20 -0700

    NUAttribute missed out code (#50)

--- NUAttribute.js
+++ NUAttribute.js
@@ -58,7 +58,7 @@ export default class NUAttribute extends NUObject {
     */
     validate(...args) {
         const attrObj = args[0];
-        const attrValue = args[1][attrObj.name];
+        const attrValue = this[attrObj.name];
         //if STRING use !attrValue to check if value provided. For all other attribute types use !undefined and !null
         if (attrObj.isRequired && ((attrObj.attributeType === NUAttribute.ATTR_TYPE_STRING && !attrValue) ||
             (attrObj.attributeType !== NUAttribute.ATTR_TYPE_STRING && (attrValue === undefined || attrValue === null)))) {
