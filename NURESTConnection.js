aceca4f1207215e764be56f40746b9456ed2f194
Karthigeyan Illancheran <killanch@gmail.com>
2018-12-05 16:07:09 -0800

    handle 401 response (#74)
    * handle 401
    
    * handle error messg

--- NURESTConnection.js
+++ NURESTConnection.js
@@ -90,7 +90,9 @@ export default class NURESTConnection extends NUObject {
                     this.interceptor.success(result);
                     return result;
                 } else if (authFailure) {
-                    return result;
+                    getLogger().error(`<<<< Authentication Failure: ${response.status}: ${response.statusText}`);
+                    this.interceptor.fail(`${response.status}: ${response.statusText}`);
+                    return Promise.reject(result);
                 } else if (response.status === 403) {
                     getLogger().error(`<<<< Authentication Failure: ${response.status}`);
                     this.interceptor.onAuthenticationFailure({response, result});
