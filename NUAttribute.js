6606f7420ea1536a67aebbf6f2187790ac875200
Karthigeyan Illancheran <killanch@gmail.com>
2019-02-27 13:16:58 -0800

    fix list attribute validation (#102)
    * fix list attribute validation
    
    * addressed comment - now using getClassName

--- NUAttribute.js
+++ NUAttribute.js
@@ -1,7 +1,7 @@
 import NUObject from './NUObject';
 import NUAttributeValidationError from './NUAttributeValidationError';
 import NUException from './NUException';
-
+import { getLogger } from "./Logger";
 
 /*
   This class models an attribute object
@@ -102,8 +102,14 @@ export default class NUAttribute extends NUObject {
                 dataTypeMismatch = false;        
             if (attrObj.subType === NUAttribute.ATTR_TYPE_INTEGER || attrObj.subType === NUAttribute.ATTR_TYPE_FLOAT) {
                 dataTypeMismatch = (typeof listElementValue !== 'number');
-            } else if (attrObj.subType !== NUAttribute.ATTR_TYPE_ENUM && typeof listElementValue === 'object' && !(listElementValue instanceof attrObj.subType)) {
-                dataTypeMismatch = true;
+            } else if (attrObj.subType !== NUAttribute.ATTR_TYPE_ENUM && typeof listElementValue === 'object') {
+                if (attrObj.subType && attrObj.subType.getClassName) {
+                    if (listElementValue && listElementValue.getClassName && listElementValue.getClassName() !== attrObj.subType.getClassName()) {
+                        dataTypeMismatch = true;
+                    }
+                } else {
+                    getLogger().warn(`Invalid subType for attribute: ${attrObj.name}, subType: ${attrObj.subType}`);
+                }
             }
             if (dataTypeMismatch){
                 return new NUAttributeValidationError(attrObj.localName, attrObj.remoteName,
