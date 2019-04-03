9e7c0fb71203f1ede1ad5c234063119049019246
Karthigeyan Illancheran <karthigeyan.illancheran@nokia.com>
2019-04-02 17:32:01 -0700

    pass query param to indicate light GET

--- NUService.js
+++ NUService.js
@@ -225,10 +225,11 @@ export default class NUService extends NUObject {
       Returns an object {data: an array of NUEntity objects,
       headers: an object with properties page, pageSize, filter, orderBy, and count}
     */
-    fetchAll(RESTResourceName, parentEntity, page = 0, filter = null, orderBy = null, filterType = undefined) {
+    fetchAll(RESTResourceName, parentEntity, page = 0, filter = null, orderBy = null, filterType = undefined, light = true) {
         const EntityClass = ServiceClassRegistry.entityClassForResourceName(RESTResourceName);
+        const url = `${this.buildURL(null, RESTResourceName, parentEntity)}/?light=${light}`;
         return this.invokeRequest(
-            'GET', this.buildURL(null, RESTResourceName, parentEntity), this.computeHeaders(page, filter, orderBy, filterType)).then((response) => {
+            'GET', url, this.computeHeaders(page, filter, orderBy, filterType)).then((response) => {
                 let data = [];
 
                 if (response.data) {
