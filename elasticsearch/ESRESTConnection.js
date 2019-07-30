81e49c9617a9d2d5e4b5a032953713eb379e48dd
nxanil <anil@nodexperts.com>
2019-07-30 21:43:40 +0530

    elastic search filter

--- elasticsearch/ESRESTConnection.js
+++ elasticsearch/ESRESTConnection.js
@@ -3,6 +3,7 @@ import _ from 'lodash';
 
 import NUObject from '../NUObject';
 import { SCROLL_TIME } from './ESService';
+import objectPath from 'object-path';
 
 /*
   This class implements the HTTP actions invoked on the server
@@ -83,5 +84,32 @@ export default class ESRESTConnection extends NUObject {
     getSearchData(query, scroll) {
         const updatedQuery = scroll ? { ...query, scroll: SCROLL_TIME } : query;
         return this.client.search(updatedQuery);
+    }
+
+    async getMapping(query){
+        const index = objectPath.get(query,['query', 'index']);
+        const mapping = await this.client.indices.getMapping({index: index});
+        return this.getESColumns(objectPath.get(mapping, [index, 'mappings', 'nuage_doc_type','properties'])) || [];
+    }
+
+    getESColumns(data, parentKey = null, columns = []) {
+        if (parentKey !== null && data[0] !== "nested") {
+            data = data[0];
+        } else if (data[0] === "nested") {
+            data = data[1];
+        }
+
+        Object.entries(data).forEach(([key, val]) => {
+            if (objectPath.has(val, 'properties')) {
+                this.getESColumns(Object.values(val), parentKey ? `${parentKey}.${key}` : key, columns);
+            } else {
+                if (parentKey === null) {
+                    columns.push(key);
+                } else {
+                    columns.push(`${parentKey}.${key}`);
+                }
+            }
+        });
+        return columns;
     }
 }
