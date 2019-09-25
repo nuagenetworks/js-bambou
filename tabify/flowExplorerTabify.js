7631583da5d09b6dd11050112349531a50998de8
Karthigeyan Illancheran <karthigeyan.illancheran@nokia.com>
2019-09-25 11:37:28 -0700

    PROD-9951 - custom tabify for flow explorer

--- tabify/flowExplorerTabify.js
+++ tabify/flowExplorerTabify.js
@@ -10,7 +10,7 @@ export default class FlowExplorerTabify extends ESTabify {
             table = response.hits.hits.map((d) => (d._source));
         }
         else {
-            const errorMessage = "Tabify() invoked with invalid result set. Result set must have'hits' defined.";
+            const errorMessage = "Tabify() invoked with invalid result set. Result set must have 'hits' defined.";
             getLogger().error(errorMessage);
             throw new Error(errorMessage);
         }
