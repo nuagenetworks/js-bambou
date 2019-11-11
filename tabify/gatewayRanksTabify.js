b1e881245c351af98454e3ede24a5bf41c8179dc
Karthigeyan Illancheran <karthigeyan.illancheran@nokia.com>
2019-11-11 10:37:24 -0800

    update rank tabify

--- tabify/gatewayRanksTabify.js
+++ tabify/gatewayRanksTabify.js
@@ -1,15 +1,19 @@
-import ESTabify from '../elasticsearch/ESTabify';
+import ESTabify from "../elasticsearch/ESTabify";
+import isEmpty from "lodash/isEmpty";
 
 export default class GatewayRanksTabify extends ESTabify {
 
     process(response) {
         const result =  super.process(response) || [];
         const computedItems = [];
-        for (let rank = 1; rank <= 4; rank++) {
-            if (!result.find((item) => item && item.GatewayRank === rank)) {
-                computedItems.push({GatewayRank: rank, NSGCount: 0});
+        if (!isEmpty(result)) {
+            for (let rank = 1; rank <= 4; rank++) {
+                if (!result.find((item) => item && item.GatewayRank === rank)) {
+                    computedItems.push({GatewayRank: rank, NSGCount: 0});
+                }
             }
+            return [...result, ...computedItems].sort((item1, item2) => (item2.GatewayRank - item1.GatewayRank));
         }
-        return [...result, ...computedItems].sort((item1, item2) => (item2.GatewayRank - item1.GatewayRank));
+        return result;
     }
 }
