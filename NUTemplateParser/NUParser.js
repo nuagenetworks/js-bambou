3e2c14f11230af5713a220e06a03af7656e7f46b
nxanil <anil@nodexperts.com>
2019-11-21 17:16:11 +0530

    [Fix]: add support for array

--- NUTemplateParser/NUParser.js
+++ NUTemplateParser/NUParser.js
@@ -82,8 +82,8 @@ const Template = (fn, parameters) => {
 const parseString = (() => {
 
     // This regular expression detects instances of the
-    // template parameter syntax such as {{foo}} or {{foo:someDefault}}.
-    const regex = /{{(\w|:|\s|-|\.|\)|\(|'|,)+}}/g;
+    // template parameter syntax such as {{foo}}, {{foo:someDefault}} or {{foo:['someDefault']}}.
+    const regex = /{{(\w|:|\s|-|\.|\[|\)|\(|'|,|])+}}/g;
 
     return (str) => {
         if (regex.test(str)) {
