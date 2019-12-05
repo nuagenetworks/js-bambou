afe503f020cac578da17698494fd834c07c2501f
Karthigeyan Illancheran <killanch@gmail.com>
2019-12-05 13:35:53 -0800

    VSD-39001 UBR gw selection - tabify changes (#148) (#151)
    * ubr gw selection - tabify changes
    
    * update tabify to ensure rank 0 at the last
    
    * replace var with const

--- tabify/gatewayRanksTabify.js
+++ tabify/gatewayRanksTabify.js
@@ -2,6 +2,7 @@ import ESTabify from "../elasticsearch/ESTabify";
 import isEmpty from "lodash/isEmpty";
 
 /*
+    Tabifies into Object with ranks as keys and NSG counts as values
     Adds data for missing GatewayRank items, with NSGCount value as 0
     Sorts by Rank 1 to 4 (4 to 1 as per data received from elasticsearch response)
  */
@@ -9,14 +10,24 @@ export default class GatewayRanksTabify extends ESTabify {
 
     process(response) {
         const result =  super.process(response) || [];
-        const computedItems = [];
         if (!isEmpty(result)) {
+            const resultByRank = {};
+            result.forEach(({GatewayRank}) => {
+                resultByRank[GatewayRank] = (resultByRank[GatewayRank] || 0) + 1;
+            });
+            const resultArray = [];
+            for (const GatewayRank in resultByRank) {
+                resultArray.push({GatewayRank, NSGCount: resultByRank[GatewayRank]});
+            }
+            const computedItems = [];
             for (let rank = 0; rank <= 4; rank++) {
-                if (!result.find((item) => item && item.GatewayRank === rank)) {
+                if (!resultArray.find((item) => item && item.GatewayRank === rank)) {
                     computedItems.push({GatewayRank: rank, NSGCount: 0});
                 }
             }
-            return [...result, ...computedItems].sort((item1, item2) => (item2.GatewayRank - item1.GatewayRank));
+            return [...resultArray, ...computedItems].sort((item1, item2) => (
+                item1.GatewayRank === 0 ? 1 : item2.GatewayRank === 0 ? -1 : item1.GatewayRank - item2.GatewayRank
+            ));
         }
         return result;
     }
