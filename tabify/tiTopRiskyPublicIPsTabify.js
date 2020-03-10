387c33f5bab1fde57aae1b82bd69f0e0e7f6a2b5
Chaitanya Halbe <chaitanya.halbe@nuagenetworks.net>
2020-03-10 16:11:18 -0700

    Code refactor

--- tabify/tiTopRiskyPublicIPsTabify.js
+++ tabify/tiTopRiskyPublicIPsTabify.js
@@ -1,4 +1,5 @@
 import isEmpty from "lodash/isEmpty";
+import union from "lodash/union";
 
 /*
  *Returns the top risky public IPs coming from source and destination IPs by flows aggregations
@@ -9,14 +10,29 @@ export default class TiTopRiskyPublicIPsTabify {
         const aggregations = response && response.aggregations;
         if (!isEmpty(aggregations)) {
             const result = aggregations.srcPublicIPs && aggregations.srcPublicIPs.buckets 
-                ? aggregations.srcPublicIPs.buckets.map(item => { return {publicIP: item.key, CardinalityOf: item.CardinalityOf.value} }) : [];
+                ? aggregations.srcPublicIPs.buckets.map(item => { 
+                    return {
+                        publicIP: item.key,
+                        CardinalityOf: item.CardinalityOf.value,
+                        flowIds: item.flowIds.buckets ? item.flowIds.buckets.map((flowId) => flowId.key) : []
+                    }
+                }) : [];
             const dstPublicIPs = aggregations.dstPublicIPs && aggregations.dstPublicIPs.buckets 
-                ? aggregations.dstPublicIPs.buckets.map(item => { return {publicIP: item.key, CardinalityOf: item.CardinalityOf.value} }) : [];
+                ? aggregations.dstPublicIPs.buckets.map(item => { 
+                    return {
+                        publicIP: item.key,
+                        CardinalityOf: item.CardinalityOf.value,
+                        flowIds: item.flowIds.buckets ? item.flowIds.buckets.map((flowId) => flowId.key) : []
+                    }
+                }) : [];
+
             if (!isEmpty(dstPublicIPs)) {
                 dstPublicIPs.forEach((dstPublicIPsItem) => {
                     const resultItem = result.find(item => item.publicIP === dstPublicIPsItem.publicIP);
                     if (resultItem) {
-                        resultItem.CardinalityOf += dstPublicIPsItem.CardinalityOf
+                        const uniqueFlowIds = union(resultItem.flowIds, dstPublicIPsItem.flowIds);
+                        resultItem.flowIds = uniqueFlowIds;
+                        resultItem.CardinalityOf = uniqueFlowIds.length;
                     } else {
                         result.push({...dstPublicIPsItem});
                     }
--- tabify/tiTopSecCatsTabify.js
+++ tabify/tiTopSecCatsTabify.js
@@ -1,4 +1,5 @@
 import isEmpty from "lodash/isEmpty";
+import union from "lodash/union";
 
 /*
  *Returns the top security categories coming from source and destination IP Categories by flows aggregations
@@ -9,14 +10,29 @@ export default class TiTopSecCatsTabify {
         const aggregations = response && response.aggregations;
         if (!isEmpty(aggregations)) {
             const result = aggregations.srcIpCats && aggregations.srcIpCats.buckets 
-                ? aggregations.srcIpCats.buckets.map(item => { return {secCats: item.key, CardinalityOf: item.CardinalityOf.value} }) : [];
+                ? aggregations.srcIpCats.buckets.map(item => { 
+                    return {
+                        secCats: item.key,
+                        CardinalityOf: item.CardinalityOf.value,
+                        flowIds: item.flowIds.buckets ? item.flowIds.buckets.map((flowId) => flowId.key) : []
+                    }
+                }) : [];
             const dstIpCats = aggregations.dstIpCats && aggregations.dstIpCats.buckets 
-                ? aggregations.dstIpCats.buckets.map(item => { return {secCats: item.key, CardinalityOf: item.CardinalityOf.value} }) : [];
+                ? aggregations.dstIpCats.buckets.map(item => { 
+                    return {
+                        secCats: item.key,
+                        CardinalityOf: item.CardinalityOf.value,
+                        flowIds: item.flowIds.buckets ? item.flowIds.buckets.map((flowId) => flowId.key) : []
+                    }
+                }) : [];
+
             if (!isEmpty(dstIpCats)) {
                 dstIpCats.forEach((dstIpCatsItem) => {
-                    const resultItem = result.find(item => item.publicIP === dstIpCatsItem.publicIP);
+                    const resultItem = result.find(item => item.secCats === dstIpCatsItem.secCats);
                     if (resultItem) {
-                        resultItem.CardinalityOf += dstIpCatsItem.CardinalityOf
+                        const uniqueFlowIds = union(resultItem.flowIds, dstIpCatsItem.flowIds);
+                        resultItem.flowIds = uniqueFlowIds;
+                        resultItem.CardinalityOf = uniqueFlowIds.length;
                     } else {
                         result.push({...dstIpCatsItem});
                     }
