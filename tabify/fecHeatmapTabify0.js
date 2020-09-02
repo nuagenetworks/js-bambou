eb69a1d06434b141916b5dce1e74746356f433f9
Chaitanya Halbe <chaitanya.halbe@nuagenetworks.net>
2020-09-02 14:35:46 -0700

    Refactor

--- tabify/fecHeatmapTabify0.js
+++ tabify/fecHeatmapTabify0.js
@@ -27,12 +27,12 @@ export default class FecHeatmapTabify0 {
         if (!isEmpty(aggregations)) {
             const result = [];
             const getFECHeatmapColorValue = (key) => {
-                return !key ? 'EMPTY' : key >= 0.0 && key < 0.5 ? '0.0% - 0.499%' : key >= 0.5 && key < 2.0 ? '0.5% - 1.99%' : key >= 2.0 && key < 4.0 ? '2.0% - 3.99%' : key >= 4.0 && key < 10.0 ? '4.0% - 9.99%' : '>= 10.0%';
+                return key >= 0.0 && key < 0.5 ? '0.0% - 0.499%' : key >= 0.5 && key < 2.0 ? '0.5% - 1.99%' : key >= 2.0 && key < 4.0 ? '2.0% - 3.99%' : key >= 4.0 && key < 10.0 ? '4.0% - 9.99%' : '>= 10.0%';
             };
             if (aggregations.date_histo0 && aggregations.date_histo0.buckets) {
                 for (const dateHistoEntry of aggregations.date_histo0.buckets) {
-                    const networkLossValue = dateHistoEntry.NetworkLoss && dateHistoEntry.NetworkLoss.value;
-                    const lossAfterFecValue = dateHistoEntry.LossAfterFEC && dateHistoEntry.LossAfterFEC.value;
+                    const networkLossValue = dateHistoEntry.NetworkLoss && dateHistoEntry.NetworkLoss.value || 0.0;
+                    const lossAfterFecValue = dateHistoEntry.LossAfterFEC && dateHistoEntry.LossAfterFEC.value || 0.0;
                     result.push({
                         key_as_string: dateHistoEntry.key_as_string,
                         date_histo: dateHistoEntry.key,
