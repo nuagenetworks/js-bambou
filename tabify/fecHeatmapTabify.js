e46d48c00dfc7735ab4ec5dc9b3c8d66743ab971
Chaitanya Halbe <chaitanya.halbe@nuagenetworks.net>
2020-10-02 13:04:41 -0700

    Changing the arrow function

--- tabify/fecHeatmapTabify.js
+++ tabify/fecHeatmapTabify.js
@@ -22,12 +22,12 @@ import isEmpty from "lodash/isEmpty";
  */
 export default class FecHeatmapTabify {
 
-    getFECHeatmapColorValue = (key) => {
+    getFECHeatmapColorValue(key) {
         return key >= 0.0 && key < 0.5 ? '0.0% - 0.499%' : 
             key >= 0.5 && key < 2.0 ? '0.5% - 1.99%' : 
             key >= 2.0 && key < 4.0 ? '2.0% - 3.99%' : 
             key >= 4.0 && key < 10.0 ? '4.0% - 9.99%' : '>= 10.0%';
-    };
+    }
     
     process(response) {
         const aggregations = response && response.aggregations;
