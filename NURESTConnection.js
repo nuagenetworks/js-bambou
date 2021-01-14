255cb9f7d348e2e2767c08ee158318ca97ab9b91
preshitg <preshit.gandhi@nuagenetworks.net>
2021-01-14 11:04:30 -0800

    Pass verb and body to onMultipleChoices

--- NURESTConnection.js
+++ NURESTConnection.js
@@ -189,7 +189,7 @@ export default class NURESTConnection extends NUObject {
 
             //handle response with choice
             if (response.status === 300 && this._onMultipleChoices) {
-                return this._onMultipleChoices(response.data, requestURL)
+                return this._onMultipleChoices(response.data, requestURL, verb, body)
                     .then(choice => this.makeRequest({requestURL, verb, headers, body, choice, cancelToken}));
             }
 
