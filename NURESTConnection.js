6fb3b6c5eff6652408c1b4e550be1f9596fe5ce7
Anitha <anitha.pillai@gmail.com>
2021-03-05 20:40:11 -0800

    Handle 503 - Service unavailable err - r20.10

--- NURESTConnection.js
+++ NURESTConnection.js
@@ -32,6 +32,7 @@ class ResponseCodeEnum extends Enum {
             case 404:
             case 405:
             case 500:
+            case 503:
                 if (response && response.data && Array.isArray(response.data.errors)) {
                     return {data: response.data};
                 }
@@ -117,7 +118,12 @@ ResponseCodeEnum.initEnum({
     },
     ServiceUnavailable: {
         get code() { return 503; },
-        get errors() { return {}; }
+        get errors() {
+            return {
+                title: "Service Unavailable",
+                description: "System is in maintenance mode. Please try after sometime"
+            }
+        }
     },
 });
 
