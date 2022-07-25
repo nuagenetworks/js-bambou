1ec9b8c3702cbb527f9570eb0b4fe10a49c88f42
shashank-nx <shashank.dixit@successive.tech>
2022-07-25 15:31:45 +0530

    modify tabify file

--- tabify/rrtTabify.js
+++ tabify/rrtTabify.js
@@ -8,14 +8,13 @@ export default class FlowExplorerTabify extends ESTabify {
 
         if (response.hits) {
             table = response.hits.hits.map((d) => (d._source));
-        }
-        else {
+        } else {
             const errorMessage = "Tabify() invoked with invalid result set. Result set must have 'hits' defined.";
             getLogger().error(errorMessage);
             throw new Error(errorMessage);
         }
 
-        if (table[0].Tier1URLInfo) {
+        if (table.length && table[0].Tier1URLInfo) {
             table[0].Tier1URLInfo.forEach(val => {
                 val.roundTripTimeList = val.roundTripTimeList.join(', ');
             });
